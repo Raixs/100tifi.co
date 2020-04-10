f3d6b047b47a574c6ab42be2821f10428584a81e
Hans Pereira <hanspereira63@gmail.com>
2020-04-10 17:36:35 +0200

    Prueba

---
+++ src/templates/Header.js
@@ -0,0 +1,22 @@
+const Header = () => {
+    const view = `
+    <div class="Header-main">
+        <div class"Header-logo">
+            <h1>
+                <a href="/">
+                   Rick y morty AUTOGEN
+                </a>
+            </h1>
+        </div>
+        <div class"Header-nav">
+            <a href="#/about/">
+                  Sobre Hans
+            </a>
+        </div>
+    </div>
+    `;
+    return view;
+};
+
+export default Header;
+
